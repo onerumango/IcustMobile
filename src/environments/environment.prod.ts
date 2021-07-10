ca37e58525023de2553039720669868daae6386f
sakthideveloper <m.sakthideveloper@gmail.com>
2021-07-10 09:41:34 +0530

    endpoint added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,10 @@ export const environment = {
 
 
   
-  ICUST_URL: 'http://localhost:1212', // phase2 for local testing
+  // ICUST_URL: 'http://localhost:1212', // phase2 for local testing
+  ICUST_URL: "http://192.168.0.14:8081/Icust-Digital-Banking"
+  
  
-
 
 
 

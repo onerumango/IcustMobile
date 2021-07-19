e8b73c98e74907ba1af942d6a5b829486c195a64
sakthideveloper <m.sakthideveloper@gmail.com>
2021-07-19 16:11:13 +0530

    routing hash location added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+export const rootRouterConfig: Routes = [
   // {
   //   path: 'home',
   //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
@@ -101,21 +101,4 @@ const routes: Routes = [
     loadChildren: () => import('./views/account/account.module').then( m => m.AccountPageModule)
   },
   
-
-
-  
-
-
- 
-
-
-
 ];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { Camera } from '@ionic-native/camera/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { HttpClientModule } from '@angular/common/http';
+import { rootRouterConfig } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(),HttpClientModule,AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(),HttpClientModule,RouterModule.forRoot(rootRouterConfig, { useHash: true, relativeLinkResolution: 'legacy' })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy},Geolocation],
   bootstrap: [AppComponent],
 })

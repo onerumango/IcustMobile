978febc2d5926c164421574286a81333d8dd5637
preethi <kbpreethirose@gmail.com>
2021-07-20 10:22:21 +0530

    App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+export const rootRouterConfig: Routes = [
   // {
   //   path: 'home',
   //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
@@ -100,27 +100,5 @@ const routes: Routes = [
     path: 'account',
     loadChildren: () => import('./views/account/account.module').then( m => m.AccountPageModule)
   },
-  {
-    path: 'otp',
-    loadChildren: () => import('./views/otp/otp.module').then( m => m.OtpPageModule)
-  },
-
   
-
-
-  
-
-
- 
-
-
-
 ];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }

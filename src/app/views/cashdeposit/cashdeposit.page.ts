243a4e94c514f31f964d22642fe3f22c0c06882b
JiluJan <jilu.jan@rumango.com>
2021-09-17 09:27:24 +0530

    Account Balance Task

--- src/app/views/cashdeposit/cashdeposit.page.ts
+++ src/app/views/cashdeposit/cashdeposit.page.ts
@@ -419,9 +419,10 @@ export class CashdepositPage implements OnInit {
     this.api.accountBalance(event.detail.value).subscribe((accbal) => {
       console.log('backend accbal', accbal.currentBalance);
       this.valueSet(accbal.currentBalance);
-      console.log('backend accbal', accbal);
+      console.log('backend accbal', accbal.amount);
+      this.currentBalance = accbal.amount;
       console.log(this.depositForm.controls)
-      this.depositForm.controls.transactionAmount.patchValue(accbal.amount);
+      // this.depositForm.controls.transactionAmount.patchValue(accbal.amount);
       this.depositForm.controls.accountBranch.patchValue(accbal.accountBranch);
       this.depositForm.controls.transactionCurrency.patchValue(accbal.accountCurrency);
       // this.users=dropdown;

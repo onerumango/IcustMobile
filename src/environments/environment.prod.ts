bb62fcc60a9306c9d2915ecf69ecbb3648482afd
PoojaRumango <pooja.lakshmana@rumango.com>
2022-05-10 18:01:50 +0530

    setted proper accountId from response

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 
 
   
-  // ICUST_URL: 'http://localhost:1212', // phase2 for local testing
- ICUST_URL:'http://192.168.0.14:8081/Icust-Digital-Banking'
+  ICUST_URL: 'http://localhost:1212', // phase2 for local testing
+//  ICUST_URL:'http://192.168.0.14:8081/Icust-Digital-Banking'
 //  ICUST_URL:'http://192.168.0.14:9001/Icust-Digital-Banking',
   
  

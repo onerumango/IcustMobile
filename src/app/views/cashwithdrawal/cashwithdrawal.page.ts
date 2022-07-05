77bc4767aa4a25d9cc4b2a9e8a81cf01b2bd663b
sagar suryawamshi <93503940+sagarsd1@users.noreply.github.com>
2022-07-05 15:06:01 +0530

    tokencount minor change

--- src/app/views/cashwithdrawal/cashwithdrawal.page.ts
+++ src/app/views/cashwithdrawal/cashwithdrawal.page.ts
@@ -300,6 +300,7 @@ export class CashwithdrawalPage implements OnInit {
       if (modelData !== null) {
         let branch = modelData.data;
         console.log('Modal Data for branch: ', modelData.data);
+        this.tokenCount=modelData.data.data.tokenCount;
         this.slideOneForm.get('transactionBranch').patchValue(modelData.data['data'].branchName);
       }
     });
